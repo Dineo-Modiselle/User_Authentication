4bf12bf24d8d1030fbcf1652c4ef8e67696b6154
Dineo-Modiselle <dineo.modisellle@dci-student.org>
2025-04-25 16:45:47 +0200

    adding the Login page, the handleLogin function, Inputs, Forgot password section and Link and p tag, Login BUTTON, do not have account section

--- client/src/pages/EmailVerificationPage.jsx
+++ client/src/pages/EmailVerificationPage.jsx
@@ -1,5 +1,7 @@
 import { useState, useRef } from "react"
 import { useNavigate } from "react-router-dom"
+import { motion } from "framer-motion"
+
 
 function EmailVerificationPage() {
     const [code, setCode] = useState(["", "", "", "", "", ""]);
