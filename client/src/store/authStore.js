dbb7e3185361284c42be8044c5ad2c26fb8baa48
Dineo-Modiselle <dineo.modisellle@dci-student.org>
2025-05-01 15:01:42 +0200

    testing2

--- client/src/store/authStore.js
+++ client/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "https://user-authentication-dwys.onrender.com";
+const API_URL = "https://user-authentication-1-fapp.onrender.com";
 
 axios.defaults.withCredentials = true;
 
