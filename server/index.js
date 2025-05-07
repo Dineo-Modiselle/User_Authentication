4fb0d8015fa669590a2cb69456cba241f5f3437a
Dineo-Modiselle <dineo.modisellle@dci-student.org>
2025-05-07 14:06:36 +0200

    final- deploying website on render

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./config/connectDB.js";
 import authRoutes from "./routes/authRoutes.js";
 import cors from "cors";
+import path from "path";
 
 
 dotenv.config();
@@ -14,9 +15,11 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 
 
+const _dirname = path.resolve();
+
 //CORS
 app.use(cors({  
-  origin: "https://user-authentication-1-fapp.onrender.com",
+  origin: "http://localhost:5174",
   credentials: true,
 }));
 
@@ -26,6 +29,21 @@ app.use(cookieParser());
 
 //Routes
 app.use("/api/auth", authRoutes);
+
+//Serve static files
+if(process.env.NODE_ENV === "production"){
+app.use(express.static(path.join( _dirname, "/client/dist")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve( _dirname, "client", "dist", "index.html"));
+});
+
+}
+
+//Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 
 //Server
